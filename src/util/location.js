440f3d740b01e37673a83e0e65a91761d608cd19
sam-opensort <sam@opensort.io>
2023-10-10 11:38:15 +0530

    init

--- src/util/location.js
+++ src/util/location.js
@@ -1,5 +1,4 @@
 
-// const API_KEY = 'AIzaSyB8CRwQITpY_JFG33f-bwrlk-5sgJPw-6U'
 const API_KEY = ''
 export function getMapPreview(lat,lng){
   const imagePreview =`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x300&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${API_KEY}`;
